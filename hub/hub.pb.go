a908bd841d003ddf81c543cf7ed5bc17943356ee
Murat Odunc <murat.asya@gmail.com>
2019-09-15 16:25:44 +0300

    adds sessionId as new fieald

--- hub/hub.pb.go
+++ hub/hub.pb.go
@@ -52,11 +52,96 @@ func (x ConnectionErr) String() string {
 	return proto.EnumName(ConnectionErr_name, int32(x))
 }
 func (ConnectionErr) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_hub_4f5db3817b014075, []int{0}
+	return fileDescriptor_hub_8be8b914c2a60943, []int{0}
+}
+
+type RegisterRequest struct {
+	AgentId              string   `protobuf:"bytes,1,opt,name=agentId,proto3" json:"agentId,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *RegisterRequest) Reset()         { *m = RegisterRequest{} }
+func (m *RegisterRequest) String() string { return proto.CompactTextString(m) }
+func (*RegisterRequest) ProtoMessage()    {}
+func (*RegisterRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hub_8be8b914c2a60943, []int{0}
+}
+func (m *RegisterRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_RegisterRequest.Unmarshal(m, b)
+}
+func (m *RegisterRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_RegisterRequest.Marshal(b, m, deterministic)
+}
+func (dst *RegisterRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_RegisterRequest.Merge(dst, src)
+}
+func (m *RegisterRequest) XXX_Size() int {
+	return xxx_messageInfo_RegisterRequest.Size(m)
+}
+func (m *RegisterRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_RegisterRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_RegisterRequest proto.InternalMessageInfo
+
+func (m *RegisterRequest) GetAgentId() string {
+	if m != nil {
+		return m.AgentId
+	}
+	return ""
+}
+
+type RegisterResponse struct {
+	AgentId              string   `protobuf:"bytes,1,opt,name=agentId,proto3" json:"agentId,omitempty"`
+	SessionId            string   `protobuf:"bytes,2,opt,name=sessionId,proto3" json:"sessionId,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *RegisterResponse) Reset()         { *m = RegisterResponse{} }
+func (m *RegisterResponse) String() string { return proto.CompactTextString(m) }
+func (*RegisterResponse) ProtoMessage()    {}
+func (*RegisterResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hub_8be8b914c2a60943, []int{1}
+}
+func (m *RegisterResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_RegisterResponse.Unmarshal(m, b)
+}
+func (m *RegisterResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_RegisterResponse.Marshal(b, m, deterministic)
+}
+func (dst *RegisterResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_RegisterResponse.Merge(dst, src)
+}
+func (m *RegisterResponse) XXX_Size() int {
+	return xxx_messageInfo_RegisterResponse.Size(m)
+}
+func (m *RegisterResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_RegisterResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_RegisterResponse proto.InternalMessageInfo
+
+func (m *RegisterResponse) GetAgentId() string {
+	if m != nil {
+		return m.AgentId
+	}
+	return ""
+}
+
+func (m *RegisterResponse) GetSessionId() string {
+	if m != nil {
+		return m.SessionId
+	}
+	return ""
 }
 
 type ReadRequest struct {
-	Size                 uint32   `protobuf:"varint,1,opt,name=size,proto3" json:"size,omitempty"`
+	SessionId            string   `protobuf:"bytes,1,opt,name=sessionId,proto3" json:"sessionId,omitempty"`
+	Size                 uint32   `protobuf:"varint,2,opt,name=size,proto3" json:"size,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -66,7 +151,7 @@ func (m *ReadRequest) Reset()         { *m = ReadRequest{} }
 func (m *ReadRequest) String() string { return proto.CompactTextString(m) }
 func (*ReadRequest) ProtoMessage()    {}
 func (*ReadRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hub_4f5db3817b014075, []int{0}
+	return fileDescriptor_hub_8be8b914c2a60943, []int{2}
 }
 func (m *ReadRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ReadRequest.Unmarshal(m, b)
@@ -85,6 +170,13 @@ func (m *ReadRequest) XXX_DiscardUnknown() {
 }
 
 var xxx_messageInfo_ReadRequest proto.InternalMessageInfo
+
+func (m *ReadRequest) GetSessionId() string {
+	if m != nil {
+		return m.SessionId
+	}
+	return ""
+}
 
 func (m *ReadRequest) GetSize() uint32 {
 	if m != nil {
@@ -106,7 +198,7 @@ func (m *ReadResponse) Reset()         { *m = ReadResponse{} }
 func (m *ReadResponse) String() string { return proto.CompactTextString(m) }
 func (*ReadResponse) ProtoMessage()    {}
 func (*ReadResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hub_4f5db3817b014075, []int{1}
+	return fileDescriptor_hub_8be8b914c2a60943, []int{3}
 }
 func (m *ReadResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ReadResponse.Unmarshal(m, b)
@@ -148,7 +240,8 @@ func (m *ReadResponse) GetErr() ConnectionErr {
 }
 
 type WriteRequest struct {
-	Data                 []byte   `protobuf:"bytes,1,opt,name=data,proto3" json:"data,omitempty"`
+	SessionId            string   `protobuf:"bytes,1,opt,name=sessionId,proto3" json:"sessionId,omitempty"`
+	Data                 []byte   `protobuf:"bytes,2,opt,name=data,proto3" json:"data,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -158,7 +251,7 @@ func (m *WriteRequest) Reset()         { *m = WriteRequest{} }
 func (m *WriteRequest) String() string { return proto.CompactTextString(m) }
 func (*WriteRequest) ProtoMessage()    {}
 func (*WriteRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hub_4f5db3817b014075, []int{2}
+	return fileDescriptor_hub_8be8b914c2a60943, []int{4}
 }
 func (m *WriteRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_WriteRequest.Unmarshal(m, b)
@@ -177,6 +270,13 @@ func (m *WriteRequest) XXX_DiscardUnknown() {
 }
 
 var xxx_messageInfo_WriteRequest proto.InternalMessageInfo
+
+func (m *WriteRequest) GetSessionId() string {
+	if m != nil {
+		return m.SessionId
+	}
+	return ""
+}
 
 func (m *WriteRequest) GetData() []byte {
 	if m != nil {
@@ -197,7 +297,7 @@ func (m *WriteResponse) Reset()         { *m = WriteResponse{} }
 func (m *WriteResponse) String() string { return proto.CompactTextString(m) }
 func (*WriteResponse) ProtoMessage()    {}
 func (*WriteResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hub_4f5db3817b014075, []int{3}
+	return fileDescriptor_hub_8be8b914c2a60943, []int{5}
 }
 func (m *WriteResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_WriteResponse.Unmarshal(m, b)
@@ -243,7 +343,7 @@ func (m *Addr) Reset()         { *m = Addr{} }
 func (m *Addr) String() string { return proto.CompactTextString(m) }
 func (*Addr) ProtoMessage()    {}
 func (*Addr) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hub_4f5db3817b014075, []int{4}
+	return fileDescriptor_hub_8be8b914c2a60943, []int{6}
 }
 func (m *Addr) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Addr.Unmarshal(m, b)
@@ -278,6 +378,8 @@ func (m *Addr) GetString_() string {
 }
 
 func init() {
+	proto.RegisterType((*RegisterRequest)(nil), "hub.RegisterRequest")
+	proto.RegisterType((*RegisterResponse)(nil), "hub.RegisterResponse")
 	proto.RegisterType((*ReadRequest)(nil), "hub.ReadRequest")
 	proto.RegisterType((*ReadResponse)(nil), "hub.ReadResponse")
 	proto.RegisterType((*WriteRequest)(nil), "hub.WriteRequest")
@@ -298,6 +400,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ConnectionClient interface {
+	Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*ReadResponse, error)
 	Read(ctx context.Context, in *ReadRequest, opts ...grpc.CallOption) (*ReadResponse, error)
 	Write(ctx context.Context, in *ReadRequest, opts ...grpc.CallOption) (*ReadResponse, error)
 	Close(ctx context.Context, in *ReadRequest, opts ...grpc.CallOption) (*ReadResponse, error)
@@ -309,6 +412,15 @@ type connectionClient struct {
 
 func NewConnectionClient(cc *grpc.ClientConn) ConnectionClient {
 	return &connectionClient{cc}
+}
+
+func (c *connectionClient) Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*ReadResponse, error) {
+	out := new(ReadResponse)
+	err := c.cc.Invoke(ctx, "/hub.Connection/Register", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *connectionClient) Read(ctx context.Context, in *ReadRequest, opts ...grpc.CallOption) (*ReadResponse, error) {
@@ -340,6 +452,7 @@ func (c *connectionClient) Close(ctx context.Context, in *ReadRequest, opts ...g
 
 // ConnectionServer is the server API for Connection service.
 type ConnectionServer interface {
+	Register(context.Context, *RegisterRequest) (*ReadResponse, error)
 	Read(context.Context, *ReadRequest) (*ReadResponse, error)
 	Write(context.Context, *ReadRequest) (*ReadResponse, error)
 	Close(context.Context, *ReadRequest) (*ReadResponse, error)
@@ -347,6 +460,24 @@ type ConnectionServer interface {
 
 func RegisterConnectionServer(s *grpc.Server, srv ConnectionServer) {
 	s.RegisterService(&_Connection_serviceDesc, srv)
+}
+
+func _Connection_Register_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RegisterRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConnectionServer).Register(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/hub.Connection/Register",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConnectionServer).Register(ctx, req.(*RegisterRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _Connection_Read_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -408,6 +539,10 @@ var _Connection_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*ConnectionServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
+			MethodName: "Register",
+			Handler:    _Connection_Register_Handler,
+		},
+		{
 			MethodName: "Read",
 			Handler:    _Connection_Read_Handler,
 		},
@@ -424,30 +559,34 @@ var _Connection_serviceDesc = grpc.ServiceDesc{
 	Metadata: "hub.proto",
 }
 
-func init() { proto.RegisterFile("hub.proto", fileDescriptor_hub_4f5db3817b014075) }
+func init() { proto.RegisterFile("hub.proto", fileDescriptor_hub_8be8b914c2a60943) }
 
-var fileDescriptor_hub_4f5db3817b014075 = []byte{
-	// 339 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0xc1, 0x4f, 0xc2, 0x30,
-	0x14, 0xc6, 0xed, 0x36, 0x41, 0x9e, 0x8c, 0x8c, 0x1e, 0xcc, 0xe2, 0x09, 0x17, 0x63, 0x88, 0x1a,
-	0x0e, 0x78, 0xf1, 0x2a, 0xa3, 0x06, 0xa2, 0x71, 0x4b, 0xc1, 0xe0, 0xc1, 0x0b, 0x63, 0x0d, 0x2c,
-	0xc2, 0x8a, 0x6d, 0x17, 0x13, 0xff, 0x0f, 0xff, 0x5f, 0xd3, 0x3a, 0x03, 0x26, 0x86, 0x70, 0x7b,
-	0xef, 0xeb, 0x97, 0xdf, 0x7b, 0xf9, 0xfa, 0xa0, 0xb6, 0x28, 0x92, 0xce, 0x5a, 0x70, 0xc5, 0xb1,
-	0xbd, 0x28, 0x92, 0xe0, 0x0c, 0x8e, 0x29, 0x9b, 0xa6, 0x94, 0xbd, 0x17, 0x4c, 0x2a, 0x8c, 0xc1,
-	0x91, 0xd9, 0x27, 0xf3, 0x51, 0x0b, 0xb5, 0x5d, 0x6a, 0xea, 0xe0, 0x15, 0xea, 0x3f, 0x16, 0xb9,
-	0xe6, 0xb9, 0x64, 0xff, 0x79, 0xb4, 0x96, 0x4e, 0xd5, 0xd4, 0xb7, 0x5a, 0xa8, 0x5d, 0xa7, 0xa6,
-	0xc6, 0xe7, 0x60, 0x33, 0x21, 0x7c, 0xbb, 0x85, 0xda, 0x8d, 0x2e, 0xee, 0xe8, 0xc1, 0x21, 0xcf,
-	0x73, 0x36, 0x53, 0x19, 0xcf, 0x89, 0x10, 0x54, 0x3f, 0x07, 0x01, 0xd4, 0x27, 0x22, 0x53, 0x6c,
-	0x6b, 0x03, 0x43, 0x42, 0x1b, 0x52, 0x30, 0x04, 0xb7, 0xf4, 0xec, 0x58, 0xa1, 0x1c, 0x67, 0xed,
-	0x1e, 0x77, 0x0b, 0xce, 0x5d, 0x9a, 0x0a, 0xec, 0x43, 0x35, 0x67, 0xea, 0x83, 0x8b, 0x37, 0x03,
-	0xa9, 0xd1, 0xdf, 0x16, 0x9f, 0x40, 0x45, 0x2a, 0x91, 0xe5, 0x73, 0x83, 0xaa, 0xd1, 0xb2, 0xbb,
-	0x1c, 0x81, 0xfb, 0x87, 0x87, 0x2b, 0x60, 0x45, 0x0f, 0xde, 0x01, 0xae, 0x82, 0x4d, 0xa2, 0x7b,
-	0x0f, 0xe1, 0x26, 0xb8, 0xcf, 0x4f, 0xe4, 0x25, 0x26, 0xe1, 0x98, 0xf4, 0xb5, 0x64, 0xe1, 0x06,
-	0xc0, 0x68, 0x10, 0xd1, 0xf1, 0x84, 0x0e, 0xc7, 0xc4, 0xb3, 0x75, 0x1f, 0x3e, 0x46, 0x23, 0xd2,
-	0x8f, 0x87, 0x31, 0xf1, 0x9c, 0xee, 0x17, 0x02, 0xd8, 0x50, 0xf1, 0x15, 0x38, 0x3a, 0x6a, 0xec,
-	0x99, 0xf5, 0xb7, 0x3e, 0xe6, 0xb4, 0xb9, 0xa5, 0x94, 0x21, 0x5c, 0xc3, 0xa1, 0x49, 0x65, 0x6f,
-	0x77, 0xb8, 0xe4, 0x72, 0x3f, 0x77, 0xef, 0x02, 0xfc, 0x79, 0xa6, 0xb4, 0x3c, 0xe3, 0xab, 0x8e,
-	0x5c, 0xb0, 0xe5, 0x32, 0x5b, 0xc9, 0x4c, 0x1b, 0x7b, 0x47, 0x83, 0x22, 0x89, 0xf5, 0x05, 0xc5,
-	0x28, 0xa9, 0x98, 0x53, 0xba, 0xf9, 0x0e, 0x00, 0x00, 0xff, 0xff, 0x76, 0xf1, 0x82, 0x2e, 0x57,
-	0x02, 0x00, 0x00,
+var fileDescriptor_hub_8be8b914c2a60943 = []byte{
+	// 404 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x93, 0xc1, 0x6f, 0xd3, 0x30,
+	0x14, 0xc6, 0x71, 0x12, 0xba, 0xf5, 0xd1, 0x8c, 0xcc, 0x42, 0x28, 0x42, 0x1c, 0xa6, 0x08, 0xa1,
+	0x89, 0xa1, 0x1e, 0xb6, 0x0b, 0x37, 0x60, 0x99, 0xd1, 0x02, 0x88, 0x44, 0x6e, 0xd1, 0x38, 0x70,
+	0x49, 0x16, 0x2b, 0xb5, 0xe8, 0xec, 0x61, 0x3b, 0x42, 0xe2, 0x4f, 0xe4, 0xaf, 0x42, 0xf6, 0x12,
+	0xa5, 0xad, 0xd4, 0xaa, 0x37, 0xbf, 0xf7, 0x7e, 0xf9, 0xfc, 0xe5, 0xb3, 0x0d, 0xe3, 0x45, 0x5b,
+	0x4d, 0xef, 0x95, 0x34, 0x12, 0xfb, 0x8b, 0xb6, 0x4a, 0xce, 0xe0, 0x29, 0x65, 0x0d, 0xd7, 0x86,
+	0x29, 0xca, 0x7e, 0xb7, 0x4c, 0x1b, 0x1c, 0xc3, 0x41, 0xd9, 0x30, 0x61, 0xb2, 0x3a, 0x46, 0x27,
+	0xe8, 0x74, 0x4c, 0xfb, 0x32, 0xf9, 0x0c, 0xd1, 0x00, 0xeb, 0x7b, 0x29, 0x34, 0xdb, 0x4e, 0xe3,
+	0x97, 0x30, 0xd6, 0x4c, 0x6b, 0x2e, 0x45, 0x56, 0xc7, 0x9e, 0x9b, 0x0d, 0x8d, 0xe4, 0x3d, 0x3c,
+	0xa1, 0xac, 0xac, 0xfb, 0x4d, 0xd7, 0x60, 0xb4, 0x01, 0x63, 0x0c, 0x81, 0xe6, 0x7f, 0x99, 0x53,
+	0x09, 0xa9, 0x5b, 0x27, 0x3f, 0x61, 0xf2, 0x20, 0xd0, 0x19, 0xe9, 0x19, 0x34, 0x30, 0xb6, 0x57,
+	0x97, 0xa6, 0x74, 0xdf, 0x4d, 0xa8, 0x5b, 0xe3, 0x57, 0xe0, 0x33, 0xa5, 0x62, 0xff, 0x04, 0x9d,
+	0x1e, 0x9d, 0xe3, 0xa9, 0xcd, 0x23, 0x95, 0x42, 0xb0, 0x5b, 0xc3, 0xa5, 0x20, 0x4a, 0x51, 0x3b,
+	0x4e, 0x3e, 0xc0, 0xe4, 0x46, 0x71, 0xc3, 0xf6, 0xf6, 0xb7, 0xb9, 0x4f, 0x92, 0x41, 0xd8, 0x29,
+	0xec, 0x30, 0xd8, 0x99, 0xf1, 0x76, 0x9b, 0x79, 0x07, 0xc1, 0xc7, 0xba, 0x56, 0x36, 0x6b, 0xc1,
+	0xcc, 0x1f, 0xa9, 0x7e, 0xf5, 0x59, 0x77, 0x25, 0x7e, 0x0e, 0x23, 0x6d, 0x14, 0x17, 0x4d, 0x17,
+	0x74, 0x57, 0xbd, 0x99, 0x41, 0xb8, 0xa6, 0x87, 0x47, 0xe0, 0xe5, 0x5f, 0xa2, 0x47, 0xf8, 0x00,
+	0x7c, 0x92, 0x7f, 0x8a, 0x10, 0x3e, 0x86, 0xf0, 0xfb, 0x37, 0xf2, 0xa3, 0x20, 0xe9, 0x9c, 0x5c,
+	0xd9, 0x96, 0x87, 0x8f, 0x00, 0x66, 0xd7, 0x39, 0x9d, 0xdf, 0xd0, 0x6c, 0x4e, 0x22, 0xdf, 0xd6,
+	0xe9, 0xd7, 0x7c, 0x46, 0xae, 0x8a, 0xac, 0x20, 0x51, 0x70, 0xfe, 0x0f, 0x01, 0x0c, 0xaa, 0xf8,
+	0x02, 0x0e, 0xfb, 0x5b, 0x81, 0x9f, 0xb9, 0x5f, 0xd8, 0xb8, 0x51, 0x2f, 0x8e, 0xbb, 0xee, 0xca,
+	0x69, 0x9d, 0x41, 0x60, 0x6b, 0x1c, 0xad, 0x8c, 0xb6, 0xc2, 0x6f, 0xe1, 0xb1, 0x8b, 0x72, 0x6f,
+	0x3a, 0x5d, 0x4a, 0xbd, 0x1f, 0x7d, 0xf9, 0x1a, 0xe2, 0x86, 0x1b, 0xdb, 0xbe, 0x95, 0x77, 0x53,
+	0xbd, 0x60, 0xcb, 0x25, 0xbf, 0xd3, 0xdc, 0x82, 0x97, 0x87, 0xd7, 0x6d, 0x55, 0xd8, 0xb7, 0x52,
+	0xa0, 0x6a, 0xe4, 0x1e, 0xcd, 0xc5, 0xff, 0x00, 0x00, 0x00, 0xff, 0xff, 0xf4, 0xbd, 0x54, 0xc9,
+	0x41, 0x03, 0x00, 0x00,
 }
